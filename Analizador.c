e220f99e87389c76d7178beaeb1899c5b1706831
cedilloER <elias100cedillo@gmail.com>
2021-07-16 15:34:28 -0600

    Analizador
    actualizacion de analizador lexico reconoce variable

--- Analizador.c
+++ Analizador.c
@@ -7,23 +7,59 @@ char *entero[] = {""};
 char *flotante[]= {""};
 char *cadena[]= {""};
 char *luz[]= {""};
-char *fuente= {""};
+char *fuente[]= {""};
 char *funcion[]= {""};
+
+enum Testado{q0,q1,qe};
+
+void Validar(char[]);
 
 int main(){
 	
-	FILE *archivo = fopen("programa.txt", "r");
-	if(archivo == NULL){
-		printf("Archivo no leido\n");
-	}
-
-	char aux;
-	while(feof(archivo)==0){
-		fscanf(archivo, "%c", aux);
-		printf("%c", aux);
-	}
-
-	fclose(archivo);
+	char linea[200] = "int";
+	printf("Linea de codigo a complilar: %s\n\n", linea);
+	
+	Validar(linea);
+	
 
 	system("PAUSE");
 }
+
+void Validar(char palabra[]){
+    int j = strlen(palabra);
+	int estado = q0;
+	int i = 0;
+	
+	while(i<j){
+		switch(estado){
+			case q0:
+				if((palabra[i]>= 'a' && palabra[i]<='z')||(palabra[i]>= 'A' && palabra[i]<='Z')){
+					estado = q1;
+					break;
+				}else{estado=qe;}
+			case q1:
+				if((palabra[i]>= 'a' && palabra[i]<='z')||(palabra[i]>= 'A' && palabra[i]<='Z')){
+					estado = q1;
+					break;
+				}else{estado=qe;}
+		}
+		i++;
+	}
+	
+	if(estado==qe){
+		printf("Invalida\n\n");
+	}else{
+		int i;
+		int res = 0;
+		for(i=0; reservada[i]!='\0' && reservada == 0; i++){
+			if(strcmp(palabra, reservada[i])){
+				res = 1;
+				break;
+			}
+		}
+		if(res == 1){
+			printf("reservada\n\n");
+		}else{printf("variable\n\n");}
+	}
+}
+
