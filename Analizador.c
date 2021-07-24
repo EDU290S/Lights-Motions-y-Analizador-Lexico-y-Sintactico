3fc304c90f232d3837e9f632c52968f715cb30b2
cedilloER <elias100cedillo@gmail.com>
2021-07-23 19:05:30 -0600

    Avance en el analizador
    determina el tipo de linea a evaluar y separa las lineas en palabras para evaluacion

--- Analizador.c
+++ Analizador.c
@@ -3,87 +3,127 @@
 #include <string.h>
 #include <stdbool.h>
 
-char *reservada[] = {"int", "float", "String", "light", "waterjet", "if", "else", "while", "for", "range", "funtion", "return", "writeLn", "scribe", "off", "on", "intensity", "useColor", "declareColor", "pressure", "angle", "Delay", "id"};
-char *instancia[] = {"int", "float", "String", "light", "waterjet"};
+//TABLA DE SIMBOLOS
+char *reservada[] = {"int", "float", "String", "led", "waterjet", "if", "else", "while", "for", "range", "funtion", "return", "writeLn", "scribe", "off", "on", "intensity", "useColor", "declareColor", "pressure", "angle", "delay", "id"};
 
 bool esValida(char[]);
 bool esReservada(char[]);
 bool esEntero(char[]);
 bool esFlotante(char[]);
-bool esInstancia(char[]);
-void validarCadena(char[]);
+bool sintaxisCorrecta(char[]);
 
-enum type {var, fun, fr, whil, des, inst};
+void vaciar(char[]);
+void obtPalabra(char[], char[], int);
+int cuentaPalabra(char[]);
+
+enum type {var, funcion, para, delay, instancia, si, escribir, obtener, retorno, qe};
 
 char aux[100];
 
+//INICIO FUNCION MAIN
 int main(){
 
- char linea[200] = "int hola;";
- printf("Linea de codigo a complilar: %s\n\n", linea);
-
- validarCadena(linea);
-/*
-	if(esValida(linea)){
-		if(esReservada(linea)){
-			printf("Es reservada\n");
-		}else{printf("Es variable\n");}
-	}else if(esEntero(linea)){
-        printf("Es entero\n");
-	}else if(esFlotante(linea)){
-        printf("Es flotante\n");
-	}else{printf("Es invalida\n");}
-*/
-
+	char linea[200] = "  elias.asi si me gusta a yo";
+	sintaxisCorrecta(linea);
+	printf("%d\n", cuentaPalabra(linea));
 	system("PAUSE");
 }
+//FIN FUNCION MAIN
 
-void validarCadena(char linea[]){
-	int cout=0;
-	int i, tam = strlen(linea);
-	
-	//Saca la primera palabra
-	for(i = 0; i<tam; i++){
-		if(linea[i] != ' '){
-			aux[cout] = linea[i];
-			cout ++;
-		}else{
+//INICIO FUNCION PRINCIPAL PARA VALIDAR SI LA SINTAXIS DE UNA LINEA ES CORRECTA
+bool sintaxisCorrecta(char linea[]){
+	bool esCorrecta = false;
+	char aux[200] = "";
+	int q0;
+	//inicio obtiene primer palabra
+	int i = 0, a = 0, cout = 0, tam = strlen(linea);
+	while(i<tam){
+		while(i<tam && linea[i] == ' '){i++;}
+		if(i<tam){cout++;}
+		while(i<tam && linea[i] != ' ' &&  linea[i] != '.' && linea[i] != '('){
+			aux[a] = linea[i];
+			i++;
+			a++;
+		}
+		if(aux[0] != '\0'){
 			break;
-			}
+		}
 	}
-	int empieza;
-    if(esInstancia(aux)){empieza = inst;}
-    if(esValida(aux) && esReservada(aux) == false){empieza = var;}
-    
-    int j;
-	vaciar();
-	cout = 0;
+	//fin obtiene primer palabra
+	printf("Primera Palabra: .%s.\n", aux);
+	 //inicia validacion de typo de linea a evaluar
+	if(esValida(aux) == true && esReservada(aux)==false){q0 = var;}
+	if(esValida(aux)){
+		if(esReservada(aux) == true){
+            if(strcmp(aux, "int") == 0 ||
+				strcmp(aux, "float") == 0 ||
+				strcmp(aux, "string") == 0||
+				strcmp(aux, "waterjet") == 0 ||
+				strcmp(aux, "led") == 0){q0 = instancia;}
+				
+			else if(strcmp(aux, "delay") == 0){q0 = delay;}
+			
+			else if(strcmp(aux, "return") == 0){q0 = retorno;}
+			
+			else if(strcmp(aux, "while") == 0
+					|| strcmp(aux, "if") == 0){q0 = si;}
+					
+			else if(strcmp(aux, "funtion") == 0){q0 = funcion;}
+			
+			else if(strcmp(aux, "for") == 0){q0 = para;}
+			
+			else if(strcmp(aux, "writeLn") == 0){q0 = escribir;}
+			
+			else if(strcmp(aux, "scribe") == 0){q0 = obtener;}
+		}
+	}else{q0 = qe;}
+	//fin validacion de typo de linea a evaluar
 	
-    switch(empieza){
-		case inst:
-			linea[tam] = '_';
-            for(j = i+1; j<=tam; j++){
-				if(linea[j] != ';'){
-					aux[cout] = linea[j];
-					cout ++;
-				}else{break;}
-			}
-			if(esValida(aux) && esReservada(aux) == false){
-				printf("Sintaxis Correcta\n");
-			}else{printf("Sintaxis Incorrecta\n");}
-			break;
-
+	//inicio casos a evaluar
+	switch(q0){
 		case var:
-			printf("Esto es una asignacion de variable\n");
+            printf("SINTAXIS VARIABLE\n");
 			break;
+		case funcion:
+            printf("SINTAXIS FUNCION\n");
+            break;
+		case para:
+            printf("SINTAXIS FOR\n");
+            break;
+		case delay:
+            printf("SINTAXIS RETRASO\n");
+            break;
+		case instancia:
+            printf("SINTAXIS DEFINICION\n");
+            break;
+		case si:
+            printf("SINTAXIS IF O WHILE\n");
+            break;
+		case escribir:
+            printf("SINTAXIS PRINT\n");
+            break;
+		case obtener:
+            printf("SINTAXIS CIN\n");
+            break;
+		case retorno:
+            printf("SINTAXIS RETURN\n");
+            break;
+		case qe:
+			printf("SINTAXIS INVALIDA PARA INICIO DE LINEA\n");
+            break;
 	}
+	//fin casos a evaluar
 	
+	return esCorrecta;
 }
+//FIN FUNCION PRINCIPAL PARA VALIDAR SI LA SINTAXIS DE UNA LINEA ES CORRECTA
 
-void vaciar(){
-	int n, i = strlen(aux);
+
+//Vacia una cadena de caracteres
+void vaciar(char str[]){
+	int n, i = strlen(str);
 	for(n=0; n<i;n++){
-		aux[n] = '\0';
+		str[n] = '\0';
 	}
 }
 
@@ -120,18 +160,6 @@ bool esReservada(char cadena[]){
 	int i, tam = sizeof(reservada)/sizeof(char*);
 	for(i=0; i<tam && esRes == false; i++){
 		if(strcmp(cadena, reservada[i]) == 0){
-			esRes = true;
-		}
-	}
-	return esRes;
-}
-
-//Valida si es instancia
-bool esInstancia(char cadena[]){
-	bool esRes = false;
-	int i, tam = sizeof(instancia)/sizeof(char*);
-	for(i=0; i<tam && esRes == false; i++){
-		if(strcmp(cadena, instancia[i]) == 0){
 			esRes = true;
 		}
 	}
@@ -205,3 +233,33 @@ bool esEntero(char cadena[]){
 	}else{return false;}
 }
 
+//obtiene la palabra n de una cadena linea
+void obtPalabra(char linea[], char aux[], int n){
+    char aux2[100];
+	vaciar(aux);
+	int i = 0, a = 0, cout = 0, tam = strlen(linea);
+	while(i<tam && cout < n){
+        a = 0;
+		vaciar(aux2);
+		while(i<tam && linea[i] == ' '){i++;}
+		if(i<tam){cout++;}
+		while(i<tam && linea[i] != ' '){
+			aux2[a] = linea[i];
+			i++;
+			a++;
+		}
+	}
+	strcpy(aux, aux2);
+}
+
+//Cuenta las palabra en una cadena
+int cuentaPalabra(char cadena[]){
+    int i = 0, cout = 0, tam = strlen(cadena);
+	while(i<tam){
+		while(i<tam && cadena[i] == ' '){i++;}
+		if(i<tam){cout++;}
+		while(i<tam && cadena[i] != ' '){i++;}
+	}
+	
+	return cout;
+}
